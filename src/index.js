ec5241d1e3dfd83a6d7a5de72a17cc77aa00f980
anaplian <ahuckerby@hotmail.co.uk>
2017-07-29 17:49:47 +0100

    Create a data structure for the grid (#3)
    * Create a data structure for the grid
    
    * Refactor

--- src/index.js
+++ src/index.js
@@ -9,7 +9,24 @@ const Sprite = PIXI.Sprite;
 const STAGE_WIDTH_PX = 800;
 const STAGE_HEIGHT_PX = 494;
 
-      
+const GRID_WIDTH = 10;
+const GRID_HEIGHT = 10;
+
+const ORIENTATIONS = {
+  NORTH: 0,
+  EAST: 1,
+  SOUTH: 2,
+  WEST: 3
+};
+
+const TYPES = {
+  A: 0, B: 1, C: 2, D: 3
+}
+
+function randomProperty (obj) {
+    const keys = Object.keys(obj)
+    return obj[keys[ keys.length * Math.random() << 0]];
+};
 
 //Create a Pixi stage and renderer 
 const stage = new Container();
@@ -23,6 +40,7 @@ loader
 
 
 let logoSprite;
+let grid;
 
 function setup() {
   logoSprite = new Sprite(resources["img/ld-logo.png"].texture);
@@ -31,7 +49,17 @@ function setup() {
   logoSprite.x = STAGE_WIDTH_PX / 2;
   logoSprite.y = STAGE_HEIGHT_PX / 2;
   stage.addChild(logoSprite);
- 
+
+  let x, y;
+  grid = [];
+  for(x = 0; x < GRID_WIDTH; x++) {
+    grid[x] = [];
+    for(y = 0; y < GRID_HEIGHT; y++) {
+      grid[x][y] = GridTile.makeRandomTile();
+    }
+  }
+
+  console.log(grid);
   //Start the game loop
   gameLoop();
 }
@@ -40,4 +68,22 @@ function gameLoop(){
   requestAnimationFrame(gameLoop);
   logoSprite.rotation += 0.01;
   renderer.render(stage);
+}
+
+class GridTile {
+  constructor(type, orientation) {
+    this.type = type;
+    this.orientation = orientation;
+  }
+
+  rotate() {
+    this.orientation = (this.orientation + 1) % 4;
+  }
+
+  static makeRandomTile() {
+    const randomTileType = randomProperty(TYPES),
+        randomOrientation = randomProperty(ORIENTATIONS);
+
+    return new GridTile(randomTileType, randomOrientation);
+  }
 }
